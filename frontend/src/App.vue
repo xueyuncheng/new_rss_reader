b06f9dbd3d9c6ba3319c88e3d74b0389b313e13f
薛运成 <datouguix@gmail.com>
2021-11-20 15:11:23 +0800

    前端更新：支持仅仅显示所选中的rss源

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,23 +2,20 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="AAAAA"/> -->
   <Feed />
-  <News />
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue';
 // import News from './components/News.vue';
-import News from './components/News.vue';
-import Feed from './components/Feed.vue';
+import Feed from "./components/Feed.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // HelloWorld,
-    News,
     Feed,
-  }
-}
+  },
+};
 </script>
 
 <style>
--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,67 +1,105 @@
 <template>
-    <div class="feed" v-for="feed in feeds" :key="feed.id">
-            <input type="checkbox" v-model="feed.id" :key="feed.id"/>
-            <label for="feed.id">{{ feed.name }}</label>
-            <button @click="deleteFeed(feed.id)" :key="feed.id">
-                删除 
-            </button>
-    </div>
+  <div class="feed" v-for="feed in feeds" :key="feed.id">
+    <input
+      type="checkbox"
+      v-model="checked_ids"
+      :key="feed.id"
+      :value="feed.id"
+    />
+    <label for="feed.id">{{ feed.name }}</label>
+    <button @click="deleteFeed(feed.id)" :key="feed.id">删除</button>
+  </div>
 
-    <form @submit.prevent="addFeed">
-        <input type="text" v-model="name" />
-        <button type="submit">Add</button>
-    </form>
+  <form @submit.prevent="addFeed(name)">
+    <input type="text" v-model="name" />
+    <button type="submit">Add</button>
+  </form>
 
-    <button @click="listFeed">刷新</button>
+  <button @click="listNews(checked_ids)">刷新</button>
+
+  <div class="news">
+    <ol>
+      <li v-for="item in newses" :key="item.id">
+        <a :href="item.link">{{ item.title }}</a>
+        [{{ item.publish_time }}]
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name: 'Feed',
-    data() {
-        return {
-            feeds: [],
-            name: '',
-        }
-    },
-    created() {
-        this.listFeed();
-    },
-    methods: {
-        listFeed() {
-            axios.get('http://localhost:10001/api/feeds')
-                .then(response => {
-                    this.feeds = response.data.data;
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
+  name: "Feed",
 
-        addFeed() {
-            axios.post('http://localhost:10001/api/feeds', {
-                name: this.name,
-            })
-            .then(() => {
-                this.listFeed();
-                this.name = '';
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
+  data() {
+    return {
+      feeds: [],
+      name: "",
+      checked_ids: [],
+      newses: [],
+    };
+  },
 
-        deleteFeed(feed_id) {
-            axios.delete(`http://localhost:10001/api/feeds/${feed_id}`)
-                .then(() => {
-                    this.listFeed();
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
+  created() {
+    this.listFeed();
+    this.listNews();
+  },
+
+  methods: {
+    listFeed() {
+      console.log(this.checked_ids);
+      axios
+        .get("http://localhost:10001/api/feeds")
+        .then((response) => {
+          this.feeds = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-}
+
+    addFeed(name) {
+      axios
+        .post("http://localhost:10001/api/feeds", {
+          name: name,
+        })
+        .then(() => {
+          this.listFeed();
+          this.name = "";
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    deleteFeed(feed_id) {
+      axios
+        .delete(`http://localhost:10001/api/feeds/${feed_id}`)
+        .then(() => {
+          this.listFeed();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    listNews(checked_ids) {
+      console.log(checked_ids);
+      axios
+        .get(`http://localhost:10001/api/news`, {
+          params: {
+            feed_ids: this.checked_ids,
+          },
+        })
+        .then((response) => {
+          this.newses = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
